4bf717ba39f28a169ffd490c0e24d1a9668a6737
yh00615 <yhkim00615@gmail.com>
2024-03-14 20:07:53 +0900

    home add - board controller

--- team_test/src/main/java/com/example/controller/BoardController.java
+++ team_test/src/main/java/com/example/controller/BoardController.java
@@ -1,5 +1,14 @@
 package com.example.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class BoardController {
+	
+	@GetMapping("/")
+	public String home(Model model) {
+		
+		return "home";
+	}
 
 }
