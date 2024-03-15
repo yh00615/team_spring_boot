eda57bab0a1066fe5eea10113f1210abee24d9f6
dp00615 <dp00615@naver.com>
2024-03-15 13:08:24 +0900

    boardDTO add- time1

---
+++ team_test/src/main/java/com/example/dto/BoardDTO.java
@@ -0,0 +1,11 @@
+package com.example.dto;
+
+
+public class BoardDTO {
+	private int board_id;
+	private String member_id;
+	private String title;
+	private String content;
+	private String write_time;
+	
+}
