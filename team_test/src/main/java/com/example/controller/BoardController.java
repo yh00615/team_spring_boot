b2aa29037ebcf47414d2b6705f24d16224bdf78d
dp00615 <dp00615@naver.com>
2024-03-15 13:33:21 +0900

    boardcontroller - add method - team1

--- team_test/src/main/java/com/example/controller/BoardController.java
+++ team_test/src/main/java/com/example/controller/BoardController.java
@@ -3,15 +3,21 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BoardController {
 	String s3Path = System.getenv("S3_PATH");
-	
+
 	@GetMapping("/")
 	public String home(Model model) {
-		
+
 		return "home";
 	}
-	
+
+	@GetMapping("/boardlist")
+	public String boardlist(@RequestParam(value = "page", required = false, defaultValue = "1") int page, Model model) {
+		return "boardlist";
+	}
+
 }
